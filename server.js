3781578d881904e61b574e0bfb703ee4ab2b2cd1
sanyakataria <katariasanya@gmail.com>
2019-10-11 22:58:36 +0530

    chat_app
    Signed-off-by: sanyakataria <katariasanya@gmail.com>

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var path = require('path');
+
+users = [];
+connections = [];
+
+app.set('view engine','hbs');
+
+app.use(express.json())
+app.use(express.urlencoded(({ extended: true })))
+
+app.use('/',express.static(__dirname+'/public'))
+
+app.get('/', (req,res) => {
+    res.render('index');
+})
+
+io.on('connection', function(socket){
+    connections.push(socket);
+    console.log("connected: %s socket(s) connected", connections.length);
+
+    //Disconnect
+    socket.on('disconnect', function(data){
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('disconnected: %s socket(s) disconnected', connections.length);
+    });
+
+    //Send Message
+    socket.on('send message', function(data){
+        console.log(data);
+        io.emit('new message', {msg: data})
+    });
+})
+
+server.listen(9999, () =>{
+    console.log("server running.... on http://localhost:9999/ ");
+});
