e34382fbeb892659f47dc018dd5e4fecaf54a2e0
sanyakataria <katariasanya@gmail.com>
2019-10-12 01:48:44 +0530

    web sockets done
    Signed-off-by: sanyakataria <katariasanya@gmail.com>

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.set('view engine','hbs');
 app.use(express.json())
 app.use(express.urlencoded(({ extended: true })))
 
-app.use('/',express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req,res) => {
     res.render('index');
@@ -25,6 +25,8 @@ io.on('connection', function(socket){
 
     //Disconnect
     socket.on('disconnect', function(data){
+        users.splice(users.indexOf(socket.username), 1)
+        updateUsernames();
         connections.splice(connections.indexOf(socket), 1);
         console.log('disconnected: %s socket(s) disconnected', connections.length);
     });
@@ -32,8 +34,20 @@ io.on('connection', function(socket){
     //Send Message
     socket.on('send message', function(data){
         console.log(data);
-        io.emit('new message', {msg: data})
+        io.emit('new message', {msg: data, user: socket.username})
     });
+
+    // new user
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    })
+
+    function updateUsernames(){
+        io.emit('get users', users);
+    }
 })
 
 server.listen(9999, () =>{
